3687bdd3f69f126025f4bb4b42d5199a3620eda2
Swati <swati17293@gmail.com>
2020-08-18 01:26:56 +0200

    Update train.py

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -22,29 +22,50 @@ def create_model(ans_dic_ot, weight_matrixx_yn, weight_matrixx_ot, wrdidx_yn, wr
     len_wrdidx_ot =len(wrdidx_ot) + 1
 
     #image
+
     encoded_image = Input(shape=(1000,))
     dense_image = Dense(dim*2)(encoded_image)
-    repeat_image = RepeatVector(maxlen)(dense_image)
+    batch_image = BatchNormalization()(dense_image)
+    repeat_image = RepeatVector(maxlen)(batch_image)
 
-    #Q&A
     encode_question_yn = Input(shape=(maxlen,))
     embed_question_yn = Embedding(len_wrdidx_yn,600,input_length=maxlen,weights=[weight_matrixx_yn],trainable=False)(encode_question_yn)
     batch_question_yn = Bidirectional(LSTM(dim, return_sequences=True,dropout=0.5))(embed_question_yn)
 
+    dic = 1000
     encode_question_ot = Input(shape=(maxlen,))
-    embed_question_ot = Embedding(len_wrdidx_ot,600,input_length=maxlen,weights=[weight_matrixx_ot],trainable=False)(encode_question_ot)
-    batch_question_ot = Bidirectional(LSTM(dim, return_sequences=True,dropout=0.5))(embed_question_ot)
+    embed_question = Embedding(input_dim=dic, output_dim=dim, input_length=maxlen)(encode_question_ot)
+    batch_question_ot = Bidirectional(LSTM(dim, return_sequences=True,dropout=0.5))(embed_question)
 
-    batch_model = concatenate([repeat_image,batch_question_yn])
-    batch_model = BatchNormalization()(batch_model)
+    #attention_yn
+    merge_attention = add([repeat_image, batch_question_yn])
+    act1_attention = Activation('tanh')(merge_attention)
+    dense1_attention = TimeDistributed(Dense(1))(act1_attention)
+    flat_attention = Flatten()(dense1_attention)
+    act2_attention = Activation('softmax')(flat_attention)
+    repeat_attention = RepeatVector(dim*2)(act2_attention)
+    permute_attention_yn = Permute((2, 1))(repeat_attention)
 
-    batch_model_yn = Dense(3)(batch_question_yn)
+    #attention_ot
+    merge_attention = add([repeat_image, batch_question_ot])
+    act1_attention = Activation('tanh')(merge_attention)
+    dense1_attention = TimeDistributed(Dense(1))(act1_attention)
+    flat_attention = Flatten()(dense1_attention)
+    act2_attention = Activation('softmax')(flat_attention)
+    repeat_attention = RepeatVector(dim*2)(act2_attention)
+    permute_attention_ot = Permute((2, 1))(repeat_attention)
+
+    #merge
+    batch_model_yn = concatenate([batch_question_yn, permute_attention_yn])
+    batch_model_yn = BatchNormalization()(batch_model_yn)
+
+    batch_model_yn = Dense(3)(batch_model_yn)
     batch_model_yn = Flatten()(batch_model_yn)
 
-    batch_model = concatenate([repeat_image,batch_question_ot])
-    batch_model = BatchNormalization()(batch_model)
+    batch_model_ot = concatenate([batch_question_ot, permute_attention_ot])
+    batch_model_ot = BatchNormalization()(batch_model_ot)
 
-    batch_model_ot = Dense(maxlen)(batch_question_ot)
+    batch_model_ot = Dense(maxlen)(batch_model_ot)
     batch_model_ot = Permute((2, 1))(batch_model_ot)
 
     #output model
@@ -73,7 +94,7 @@ def create_model(ans_dic_ot, weight_matrixx_yn, weight_matrixx_ot, wrdidx_yn, wr
 
     #train
     print('Training model yn...')
-    history_yn = vqa_model_yn.fit([train_img_yn,train_que_yn],[train_hot_yn], epochs=201, batch_size=256, callbacks=callbacks_list_yn, verbose=1)
+    history_yn = vqa_model_yn.fit([train_img_yn,train_que_yn],[train_hot_yn], epochs=300, batch_size=256, callbacks=callbacks_list_yn, verbose=1)
 
     model_json = vqa_model_yn.to_json()
     with open('models/MODEL_yn.json', "w") as json_file:
@@ -83,7 +104,7 @@ def create_model(ans_dic_ot, weight_matrixx_yn, weight_matrixx_ot, wrdidx_yn, wr
     print("Saved model yn to disk")
 
     print('Training model ot...')
-    history_ot = vqa_model_ot.fit([train_img_ot,train_que_ot],[train_hot_ot], epochs=551, batch_size=256, callbacks=callbacks_list_ot, verbose=1)
+    history_ot = vqa_model_ot.fit([train_img_ot,train_que_ot],[train_hot_ot], epochs=300, batch_size=256, callbacks=callbacks_list_ot, verbose=1)
 
     model_json = vqa_model_ot.to_json()
     with open('models/MODEL_ot.json', "w") as json_file:
